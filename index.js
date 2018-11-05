b54f30511f3cd0d0ffd2941471bc7cc09303be73
Jonathan Chin <jonathankchin@gmail.com>
2018-11-04 21:14:15 -0500

    updated get all anchor tag contents with filter

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('https://bandcamp.com');
-  const hrefs = await page.$$eval('a', anchors => [].map.call(anchors, a => a.href))
+  await page.goto('https://bandcamp.com/tag/experimental');
+  await page.waitFor('body');
+  const hrefs = await page.$$eval('a', anchors => [].map.call(anchors, a => a.href).filter((item) => item.includes('.bandcamp.com') ));
   console.log(hrefs)
   await browser.close();
 })();
