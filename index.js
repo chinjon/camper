903e4f150e94775377993e7b012ae76481d216ff
Jonathan Chin <jonathankchin@gmail.com>
2018-12-21 18:41:53 -0500

    updated project

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const puppeteer = require('puppeteer');
 
+const setUrl = require('./lib/utils/setUrl');
+const getUserParams = require('./lib/utils/getUserParams');
+
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+  console.log(getUserParams(process.argv));
   await page.goto('https://bandcamp.com/tag/experimental');
   await page.waitFor('body');
   const hrefs = await page.$$eval('a', anchors => [].map.call(anchors, a => a.href).filter((item) => item.includes('.bandcamp.com') ));
