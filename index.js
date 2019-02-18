f578dbde27f8e47733583d2b06fe5a907b5ed690
Jonathan Chin <jonathankchin@gmail.com>
2019-02-17 22:23:15 -0500

    updated script

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const puppeteer = require('puppeteer');
 const setUrl = require('./lib/utils/setUrl');
 const getUserParams = require('./lib/utils/getUserParams');
 
+const bandcampUrls = require('./data/bandcampUrls');
+
+const goToUrl = setUrl(bandcampUrls.baseUrl + '/tag/experimental');
+
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   console.log(getUserParams(process.argv));
-  await page.goto('https://bandcamp.com/tag/experimental');
+  await page.goto(goToUrl);
   await page.waitFor('body');
-  const hrefs = await page.$$eval('a', anchors => [].map.call(anchors, a => a.href).filter((item) => item.includes('.bandcamp.com') ));
+  const hrefs = await page.$$eval('a', anchors => [].map.call(anchors, a => a.href).filter((item) => item.includes(".bandcamp.com") ));
   console.log(hrefs)
   await browser.close();
 })();
