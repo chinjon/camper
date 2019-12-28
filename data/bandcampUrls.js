e5c084304e3da5b5c3d20f0a51a0e485cfcbe70d
Jonathan Chin <jonathankchin@gmail.com>
2019-12-28 16:35:43 -0500

    updated map file

--- data/bandcampUrls.js
+++ data/bandcampUrls.js
@@ -1,5 +1,10 @@
 module.exports = {
-    "baseUrl": "https://bandcamp.com",
-    "extensionUrl": ".bandcamp.com",
-    "search": "/search"
+  "baseUrl": "https://bandcamp.com",
+  "extensionUrl": ".bandcamp.com",
+  "search": "/search",
+  "sort": {
+    "sortField": "sort_field=",
+    "popularity": "pop",
+    "date": "date"
+}
 }
