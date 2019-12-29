7397b694a763443e09b97027f99b6a3e313028c9
Jonathan Chin <jonathankchin@gmail.com>
2019-12-29 14:18:55 -0500

    updated entry point, leveraged puppeteer helpers

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const setUrl = require('./lib/utils/setUrl');
 const getUserParams = require('./lib/utils/getUserParams');
 const parseParams = require('./lib/utils/parseParams.js');
 const cli = require('./cli.js');
+const puppeteerHelpers = require('./lib/puppeteerHelpers/puppeteerHelpers.js');
 
 const bandcampUrls = require('./data/bandcampUrls');
 const PARAMS = getUserParams(process.argv.slice(2));
@@ -16,11 +17,17 @@ const GO_TO_URL = setUrl(bandcampUrls.baseUrl + `/tag/${userParams[0].tag}`);
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+
   console.log(getUserParams(process.argv));
   console.log(console.log(userParams))
+  
   await page.goto(GO_TO_URL);
   await page.waitFor('body');
-  const hrefs = await page.$$eval('a', anchors => [].map.call(anchors, a => a.href).filter((item) => item.includes(".bandcamp.com")));
+  const hrefs = await puppeteerHelpers.getAllHrefs(page)
+  const bandcampHrefs = puppeteerHelpers.filterArray(hrefs, '.bandcamp.com');
+
+  console.log(bandcampHrefs)
+  
   await cli();
   await browser.close();
 })();
