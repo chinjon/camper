a88f98406af2ccca33cd4488276786e5148bbf9f
Jonathan Chin <jonathankchin@gmail.com>
2019-12-29 13:06:56 -0500

    updated entry point file to leverage param parsing module

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ const puppeteer = require('puppeteer');
 
 const setUrl = require('./lib/utils/setUrl');
 const getUserParams = require('./lib/utils/getUserParams');
+const parseParams = require('./lib/utils/parseParams.js');
+const cli = require('./cli.js');
 
 const bandcampUrls = require('./data/bandcampUrls');
 const PARAMS = getUserParams(process.argv.slice(2));
-
-const GO_TO_URL = setUrl(bandcampUrls.baseUrl + `/tag/${PARAMS.tag}`);
+const userParams = parseParams(PARAMS)
+const GO_TO_URL = setUrl(bandcampUrls.baseUrl + `/tag/${userParams[0].tag}`);
 
 // https://bandcamp.com/tag/japan?sort_field=pop
 // https://bandcamp.com/tag/japan?sort_field=date
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   console.log(getUserParams(process.argv));
+  console.log(console.log(userParams))
   await page.goto(GO_TO_URL);
   await page.waitFor('body');
-  const hrefs = await page.$$eval('a', anchors => [].map.call(anchors, a => a.href).filter((item) => item.includes(".bandcamp.com") ));
-  console.log(hrefs)
+  const hrefs = await page.$$eval('a', anchors => [].map.call(anchors, a => a.href).filter((item) => item.includes(".bandcamp.com")));
+  await cli();
   await browser.close();
 })();
