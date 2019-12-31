01a7b4976168d74ee16dc7e934bb91733b8e7c62
Jonathan Chin <jonathankchin@gmail.com>
2019-12-30 19:10:13 -0500

    added open functionality to CLI

--- cli.js
+++ cli.js
@@ -1,16 +1,20 @@
 const inquirer = require('inquirer');
+const open = require('open');
 
 const cli = async (data) => {
   await inquirer
     .prompt([
       {
-        type: 'input',
-        name: 'search query',
-        message: 'What do you want to find?'
+        type: 'rawlist',
+        name: 'links',
+        message: 'What do you want to find?',
+        choices: data
       }
     ])
     .then((answers) => {
-      console.log(answers)
+
+      open(answers.links, {app: 'firefox'})
+
     });
 }
 
